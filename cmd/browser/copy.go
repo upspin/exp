0bac9ca8825fba6f4edb0667c6938d495f480fa2
Andrew Gerrand <adg@golang.org>
2017-07-05 09:37:05 +1000

    cmd/browser: add experimental web-based Upspin browser
    DO NOT REVIEW
    
    Change-Id: Id45aaf4b32b8d0eca6b3e46ae75b4eed8f790577

---
+++ cmd/browser/copy.go
@@ -0,0 +1,78 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"upspin.io/errors"
+	"upspin.io/path"
+	"upspin.io/upspin"
+)
+
+func (s *server) copyPaths(dst upspin.PathName, srcs []upspin.PathName) error {
+	// Check that the destination exists and is a directory.
+	dstEntry, err := s.cli.Lookup(dst, true)
+	if err != nil {
+		return err
+	}
+	if !dstEntry.IsDir() {
+		return errors.E(dst, errors.NotDir)
+	}
+
+	// Iterate through sources and copy them recursively.
+	for _, src := range srcs {
+		// Lookup src, but don't follow links.
+		srcEntry, err := s.cli.Lookup(src, false)
+		if err != nil {
+			return err
+		}
+		if err := s.copyPath(dst, srcEntry); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Assume that dstDir exists and is a directory.
+func (s *server) copyPath(dstDir upspin.PathName, srcEntry *upspin.DirEntry) error {
+	srcPath, err := path.Parse(srcEntry.Name)
+	if err != nil {
+		return err
+	}
+	if srcPath.NElem() == 0 {
+		return errors.E(srcEntry.Name, errors.Str("cannot copy root"))
+	}
+	dst := path.Join(dstDir, srcPath.Elem(srcPath.NElem()-1))
+
+	switch {
+	case srcEntry.IsDir():
+		// Recurse into directories.
+		if _, err := s.cli.MakeDirectory(dst); err != nil {
+			return err
+		}
+		dir, err := s.cli.DirServer(srcEntry.Name)
+		if err != nil {
+			return err
+		}
+		des, err := dir.Glob(string(srcEntry.Name + "/*"))
+		if err != nil && err != upspin.ErrFollowLink {
+			return err
+		}
+		for _, de := range des {
+			if err := s.copyPath(dst, de); err != nil {
+				return err
+			}
+		}
+		return nil
+	case srcEntry.IsLink():
+		if _, err := s.cli.PutLink(srcEntry.Link, dst); err != nil {
+			return err
+		}
+	default:
+		if _, err := s.cli.PutDuplicate(srcEntry.Name, dst); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ cmd/browser/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+
+	"upspin.io/client"
+	"upspin.io/cloud/https"
+	"upspin.io/config"
+	"upspin.io/flags"
+	"upspin.io/upspin"
+
+	_ "upspin.io/transports"
+)
+
+var upspinfs = flag.String("upspinfs", "/u", "upspinfs `mount point`")
+
+func main() {
+	flags.Parse(flags.Server)
+
+	cfg, err := config.FromFile(flags.Config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/_upspin", newServer(cfg))
+	http.Handle("/static/", http.FileServer(http.Dir(".")))
+
+	https.ListenAndServeFromFlags(nil)
+}
+
+type server struct {
+	cfg upspin.Config
+	cli upspin.Client
+}
+
+func newServer(cfg upspin.Config) http.Handler {
+	return &server{
+		cfg: cfg,
+		cli: client.New(cfg),
+	}
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+	var resp interface{}
+	switch r.FormValue("method") {
+	case "whoami":
+		resp = struct {
+			UserName upspin.UserName
+			Upspinfs string
+		}{s.cfg.UserName(), *upspinfs}
+	case "list":
+		des, err := s.cli.Glob(r.FormValue("path") + "/*")
+		var errString string
+		if err != nil {
+			errString = err.Error()
+		}
+		resp = struct {
+			Entries []*upspin.DirEntry
+			Error   string
+		}{Entries: des, Error: errString}
+	case "mkdir":
+		_, err := s.cli.MakeDirectory(upspin.PathName(r.FormValue("path")))
+		var errString string
+		if err != nil {
+			errString = err.Error()
+		}
+		resp = struct {
+			Error string
+		}{Error: errString}
+	case "rm":
+		r.ParseForm()
+		var errString string
+		for _, p := range r.Form["paths[]"] {
+			if err := s.rm(upspin.PathName(p)); err != nil {
+				errString = err.Error()
+				break
+			}
+		}
+		resp = struct {
+			Error string
+		}{Error: errString}
+	case "copy":
+		r.ParseForm()
+		dst := upspin.PathName(r.FormValue("dest"))
+		var paths []upspin.PathName
+		for _, p := range r.Form["paths[]"] {
+			paths = append(paths, upspin.PathName(p))
+		}
+		var errString string
+		if err := s.copyPaths(dst, paths); err != nil {
+			errString = err.Error()
+		}
+		resp = struct {
+			Error string
+		}{Error: errString}
+	}
+	if err := json.NewEncoder(w).Encode(&resp); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}

645a54dd01f16e5d2a664b491134a0babd646109
Rob Pike <r@golang.org>
2017-12-07 10:46:34 +1100

    cmd/upspin-store: first cut at a storage diagnostic tool
    For now it just prints the total storage consumed.
    
    % upspin store
    Upspin-store provides subcommands for analyzing storage consumption.
    It has subcommands scandir and scanstore to scan the directory and storage servers
    and report the storage consumed by those servers.
    The set of tools will grow.
    
    Usage of upspin store:
    ...
    
    Change-Id: Ibca79c32592042d362d65241cd250ae1ddb8a0a9

---
+++ cmd/upspin-store/main.go
@@ -0,0 +1,79 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Upspin-store provides subcommands for analyzing storage consumption.
+// It has several subcommands that should be used in a way yet to be
+// determined.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"upspin.io/config"
+	"upspin.io/flags"
+	"upspin.io/subcmd"
+	"upspin.io/transports"
+	"upspin.io/version"
+)
+
+type State struct {
+	*subcmd.State
+}
+
+const help = `Upspin-store provides subcommands for analyzing storage consumption.
+It has subcommands scandir and scanstore to scan the directory and storage servers
+and report the storage consumed by those servers.
+The set of tools will grow.
+`
+
+func main() {
+	const name = "setupstorage"
+
+	log.SetFlags(0)
+	log.SetPrefix("upspin store: ")
+	flag.Usage = usage
+	flags.ParseArgsInto(flag.CommandLine, os.Args[1:], flags.Client, "version")
+
+	if flags.Version {
+		fmt.Fprint(os.Stdout, version.Version())
+		os.Exit(2)
+	}
+
+	if flag.NArg() < 1 {
+		usage()
+	}
+	s := &State{
+		State: subcmd.NewState(name),
+	}
+
+	cfg, err := config.FromFile(flags.Config)
+	if err != nil {
+		s.Exit(err)
+	}
+	transports.Init(cfg)
+	s.State.Init(cfg)
+
+	switch flag.Arg(0) {
+	case "scandir":
+		s.scanDirectories(flag.Args()[1:])
+	case "scanstore":
+		s.scanStore(flag.Args()[1:])
+	default:
+		usage()
+	}
+
+	s.ExitNow()
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, help)
+	fmt.Fprintln(os.Stderr, "Usage of upspin store:")
+	fmt.Fprintln(os.Stderr, "\tupspin [globalflags] <command> [flags] ...")
+	fmt.Fprintln(os.Stderr, "\twhere <command> is one of scandir, scanstore")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
